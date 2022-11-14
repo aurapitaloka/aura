ffe90dfde8cb7380f737985c3010c0fdb88d88ce
aurapitaloka <aurapita131@gmail.com>
2022-11-14 07:42:22 +0700

    okeii

---
+++ src/vaksin.java
@@ -0,0 +1,31 @@
+public class vaksin {
+    int jumVaksin = 3; // Pfizer, Sinapharm, Moderna
+    scatitic int totalJumlahPenerimaVaksin = 12; //diubah sesuai soal
+    public static String[] Pfizer = new String[4];
+    public static String[] Sinapharm = new String[4];
+    public static String[] Moderna = new String[4];
+
+    public static void PrintInfoPaienVaksin(){
+        int totalPfrizer=0;
+        int totalSisaKuota=0;
+        for(int index=0;index<Pfizer.length;index++)
+            if(Pfizer[index] != null){
+                totalPfrizer++;
+        }
+    }
+    int total Sinapharm=0;
+    for(int index=0;index<Sinapharm.length;index++){
+        if(Sinapharm[index] !=null){
+            totalSinopharm++;
+        }
+    }
+    int total Moderna=0;
+    for(int index=0;index<Moderna.length;index++)
+        if(Moderna[index] !=null){
+            totalModerna++;
+    }
+
+
+
+    }
+
